46e871bbee7477bc1850dcc548cf2ab65037a789
70Blunck <137204675+70Blunck@users.noreply.github.com>
2025-09-08 22:03:31 -0300

    Delete Parte 2 e 3/lib directory

--- Parte 2 e 3/lib/core/data/produtos.dart
+++
@@ -1,9 +0,0 @@
-import '../models/produto.dart';
-
-final List<Produto> produtos = [
-  Produto(id: 1, nome: "Mouse Gamer", preco: 120.0),
-  Produto(id: 2, nome: "Teclado Mecânico", preco: 300.0),
-  Produto(id: 3, nome: "Cadeira Escritório", preco: 900.0),
-  Produto(id: 4, nome: "Pen Drive 32GB", preco: 45.0),
-  Produto(id: 5, nome: "Monitor 24''", preco: 850.0),
-];
